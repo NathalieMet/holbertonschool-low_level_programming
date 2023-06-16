4445ebf75506b58e7d0da606fa9fbd69e0a8c80a
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-16 11:42:24 -0700

    ex12

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline."
+ * return 0.
+ */
+
+int main(void)
+
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return(0);
+}
+
