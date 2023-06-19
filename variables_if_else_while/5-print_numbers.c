5e3c081cd4fa8e14760c83743cf34d643f44746d
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-19 06:39:59 -0700

    ex12

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, followed
+by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int nbr = 0
+		for (nbr = '0' ; nbr <= '10' ; nbr++)
+		{
+			printf(nbr);
+		}
+	printf('\n');
+	return (0);
+}
