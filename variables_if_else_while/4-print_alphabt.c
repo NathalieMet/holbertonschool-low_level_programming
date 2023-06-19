52ca6dc76c74d7885fe900fdd0132b693cbd79e4
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-19 05:10:55 -0700

    ex12

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char characters = 'a';
+
+	for (characters = 'a' ; characters <= 'z' ; characters++)
+	{
+		if ("!%d qe", characters)
+			putchar (characters);
+	}
+		putchar ('\n');
+		return (0);
+}
