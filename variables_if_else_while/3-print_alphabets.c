78317bd4eebdbeb662c15ff99d66f39c8e4175dc
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-19 04:31:30 -0700

    ex12

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        char characters = 'a';
+
+        for (characters = 'a' ; characters <= 'z' ; characters++)
+        {
+                putchar (characters);
+        }
+
+	 char characters = 'A';
+	 for (characters = 'A' ; characters <= 'Z' ; characters++)
+        {
+                putchar (characters);
+        }
+                putchar ('\n');
+        return (0);
+}
