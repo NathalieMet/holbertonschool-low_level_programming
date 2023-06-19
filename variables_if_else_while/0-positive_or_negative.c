87ee320c0bc12bf9437cbf284a3b5c7c20fd49b9
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-19 01:59:39 -0700

    ex12

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
