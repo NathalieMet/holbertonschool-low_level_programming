05fa30e75d935ca9ed81a568303695afb87805b2
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-20 07:31:02 -0700

    commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@ void print_alphabet(void)
 {
 	char alpha = 'a';
 
-        for (alpha = 'a' ; alpha <= 'z' ; alpha++)
-        {
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
 		_putchar (alpha);
-        }
-        _putchar ('\n');
+	}
+	_putchar ('\n');
 }
