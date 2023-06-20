e9d0f4922f8c284097ae7da51309036e5009d597
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-20 06:07:45 -0700

    My first commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -  prints _putchar, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char characters = 'a';
+
+        for (characters = 'a' ; characters <= 'z' ; characters++)
+        {
+		_putchar (characters);
+        }
+        _putchar ('\n');
+}
+        return (0);
+}q
