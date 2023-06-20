083ff25faa89f5fb521de96398041df9c23fa7e2
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-20 00:49:05 -0700

    ex12

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -2,18 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - prints the alphabet in lowercase, followed by a new line.
+ * main -  prints the lowercase alphabet in reverse, followed by a new line.
  * Return: Always 0.
  */
 
 int main(void)
 {
-        char characters = 'z';
+	char characters = 'z';
 
-        for (characters = 'z' ; characters <= 'a' ; characters--)
-        {
-                putchar (characters);
-        }
-                putchar ('\n');
-        return (0);
+	for (characters = 'z' ; characters >= 'a' ; characters--)
+	{
+		putchar (characters);
+	}
+	putchar ('\n');
+	return (0);
 }
