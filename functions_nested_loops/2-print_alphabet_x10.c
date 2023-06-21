5e9a4bc62f3b807468ad5457847462efa3a08ade
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-21 01:20:05 -0700

    ex2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints 10 times the alphabet, in lowercase, followed by a
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a
  * new line.
  * Return: Always 0.
  */
