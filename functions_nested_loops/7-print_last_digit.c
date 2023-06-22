67d3bf34af5fd0e695dc10b7bed9fdba22720e59
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-22 01:35:52 -0700

    ex7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,19 @@
  * Return: Always 0.
  */
 
-int print_last_digit (int n)
+int print_last_digit(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 	int a = (n % 10);
+
 	_putchar ('0' + a);
 	return (a);
 	}
 	else
 	{
 	int b = ((n * (-1)) % 10);
+
 	_putchar ('0' + b);
 	return (b);
 	}
