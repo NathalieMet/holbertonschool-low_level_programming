fe5611182dd57e41020c8fc23f021eb8f95ef5fd
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-23 04:09:10 -0700

    ex1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+File Edit Options Buffers Tools C Help
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from
+ * 00:00 to 23:59.
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return ('a' + 'b');
+}
