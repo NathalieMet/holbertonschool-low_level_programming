9214f7bff08a7f70715c78f4e2fb3bf7f5a5779f
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-26 06:23:16 -0700

    ex4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s : variable
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+	char *end = s;
+
+	while (*end != '\0')
+	{
+		end++;
+	}
+	end--;
+	while (end >= s)
+	{
+		_putchar(*end);
+		end--;
+	}
+	putchar('\n');
+}
