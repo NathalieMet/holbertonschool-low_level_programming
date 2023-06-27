0b8e5adccfd86afe35e53114687c1020ec62cb6f
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-27 02:18:12 -0700

    ex6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 #include "main.h"
+#include "2-strlen.c"
 /**
  * puts2 - prints every other character of a string, starting with the first
  * character, followed by a new line.
@@ -11,10 +12,12 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int a = _strlen(str);
+	int b = 0;
+	while (a > b)
 	{
-		_putchar(*str);
-		str += 2;
+		_putchar(str[b]);
+		b += 2;
 	}
 	putchar('\n');
 }
