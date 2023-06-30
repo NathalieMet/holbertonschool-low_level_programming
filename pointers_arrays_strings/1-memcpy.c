13acd281554c0f7b1133f3ee18a698cc580a6395
Nathalie <nathalie.metreau@hotmail.fr>
2023-06-30 06:55:45 -0700

    ex1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy - copies memory area.
+ * @dest : varible
+ * @src : variable
+ * @n : variable
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
