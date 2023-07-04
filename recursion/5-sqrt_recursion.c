4832a1e15aeec4034b8ec551649aa33355795ae8
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-04 02:05:35 -0700

    ex5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,20 +3,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _sqrt_helper - avoid overflow
  * _sqrt_recursion - returns the natural square root of a number.
  * @n : variable
+ * @x : variable
  * Return: void
  */
+int _sqrt_helper(int n, int x);
 int _sqrt_recursion(int n)
 {
-	float q;
+	return (_sqrt_helper(n, 1));
+}
+int _sqrt_helper(int n, int x)
+{
+float q;
 
 	if (n <= 0)
 		return (-1);
-	if (n == 1)
-		return (1);
-	q = (float)n / _sqrt_recursion(n - 1);
-	if (q == _sqrt_recursion(n - 1))
+	if (x * x > n)
+		return (-1);
+	q = (float)n / x;
+	if (q == x)
 		return ((int)q);
-	return (-1);
+	return (_sqrt_helper(n, x + 1));
 }
