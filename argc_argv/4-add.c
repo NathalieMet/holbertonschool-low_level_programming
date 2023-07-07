fef43a953eeeb7d79b50720ae23662056b888442
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-07 07:23:48 -0700

    ex4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,18 +14,26 @@ int main(int argc, char *argv[])
 {
 	int result = 0;
 	int i;
-	int arg;
+	int j;
 
-	if (argc < 2)
+	if (argc == 1)
+	{
 		printf("0\n");
-	for (arg = 1; arg < argc; arg++)
-		if (!isdigit(argv[arg][0]))
-		{
-			printf("Error\n");
-			return (1);
-		}
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
-		result = result + atoi(argv[i]);
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			result = result + atoi(argv[i]);
+		}
+	}
 	printf("%d\n", result);
 	return (0);
+	}
 }
