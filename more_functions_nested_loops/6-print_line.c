5b4de2c45d2b58730b424c6ac74f676e40e876a5
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-09 11:21:41 -0700

    ex6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n : variable
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+			for (i = 0; i <= n; i++)
+			{
+				_putchar('_');
+			}
+			_putchar('\n');
+}
