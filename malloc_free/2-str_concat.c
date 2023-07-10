8d15112688967923eff8d151f60b7359b029d7fc
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-10 07:34:26 -0700

    ex2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
-
+#include "1-strdup.c"
 /**
  * str_concat - concatenates two strings.
  * char.
@@ -17,6 +17,13 @@ char *str_concat(char *s1, char *s2)
 	int j;
 	int k;
 
+	if  (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (_strdup(s2));
+	if (s2 == NULL)
+		return (_strdup(s1));
+
 	for (j = 0; s1[j] != '\0'; j++)
 	{
 	}
@@ -24,6 +31,8 @@ char *str_concat(char *s1, char *s2)
 	{
 	}
 	ar = malloc((j + k + 1));
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < j; i++)
 		ar[i] = s1[i];
 	for (i = 0; i < k; i++)
