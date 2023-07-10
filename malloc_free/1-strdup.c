34043e15bc3e3c9e36619e6e438c21d96aad397a
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-10 05:24:54 -0700

    ex1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -7,26 +7,25 @@
  * _strdup - returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
  * char.
- * @size : argument
- * @c : argument
+ * @str : variable
  * Return: char
  */
 char *_strdup(char *str)
 {
-        char *ar;
-        int i;
+	char *ar;
+	int i;
 	int j;
 
 	for (j = 0; str[j] != '\0'; j++)
-	if (j == 0)
-        {
-                return (NULL);
-        }
-        ar = malloc((j + 1) * sizeof(char));
-        if (ar == NULL)
-                return (NULL);
-        for (i = 0; i < j; i++)
-                ar[i] = str[i];
-	ar [i] = '\0';
-        return (ar);
+		if (str == NULL)
+		{
+		return (NULL);
+		}
+	ar = malloc((j + 1) * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		ar[i] = str[i];
+	ar[i] = '\0';
+	return (ar);
 }
