036aab1b67113b7952c979ccf46396d2c281ddc7
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-12 08:38:44 -0700

    ex2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints the name of the file it was compiled from, followed by a new
+ * line.
+ * Return: int
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
