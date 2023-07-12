091390ade22fb3925d5037710f1186b519e521fa
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-12 05:09:40 -0700

    ex1

---
+++ more_malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter.
+ * char.
+ * @str : variable
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	int i;
+	int j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0'; j++)
+	{
+	}
+	ar = malloc((j + 1));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i <= j; i++)
+		ar[i] = str[i];
+	ar[i] = '\0';
+	return (ar);
+}
---
+++ more_malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include "1-strdup.c"
+/**
+ * str_concat - concatenates two strings.
+ * char.
+ * @s1 : variable
+ * @s2 : variable
+ * Return: char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	int i;
+	int j;
+	int k;
+
+	if (s1 == NULL && s2 == NULL)
+		return (_strdup("\0"));
+	if (s1 == NULL)
+		return (_strdup(s2));
+	if (s2 == NULL)
+		return (_strdup(s1));
+
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+	}
+	ar = malloc((j + k + 1));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		ar[i] = s1[i];
+	for (i = 0; i < k; i++)
+		ar[j + i] = s2[i];
+
+	ar[j + i] = '\0';
+	return (ar);
+}
