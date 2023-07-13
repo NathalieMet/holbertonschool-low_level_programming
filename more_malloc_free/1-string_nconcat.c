b0b69f35b03ae877fb9fb4de3688b63d16afc56c
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-13 06:25:23 -0700

    ex1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,9 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL && s2 == NULL)
 		return (strdup("\0"));
 	if (s1 == NULL)
-		return (_strdup(s2));
+		s1 = "";
 	if (s2 == NULL)
-		return (strdup(s1));
+		s2 = "";
 
 	for (j = 0; s1[j] != '\0'; j++)
 	{
