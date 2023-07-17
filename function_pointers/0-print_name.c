ca9775d703118f9bf592a442885a2486f00b15a9
Nathalie <nathalie.metreau@hotmail.fr>
2023-07-17 06:53:33 -0700

    ex0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: pointeur
+ * @f: pointeur
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef BIBLI
+#define BIBLI
+void print_name(char *name, void (*f)(char *));
+#endif
