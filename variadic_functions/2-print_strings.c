b3d0cf6e24d9f28edfd8d31afdd3b71d3277ec41
nathalie <nathalie.metreau@hotmail.fr>
2023-07-19 13:51:38 +0200

    ex2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -28,9 +28,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			str = va_arg(args, char*);
 			if (str == NULL)
-			{
 				printf("nil");
-			}
+				else
 			printf("%s", str);
 			if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
