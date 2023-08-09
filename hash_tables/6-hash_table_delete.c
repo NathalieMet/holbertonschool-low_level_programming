3ca21cf6448acf211d278e94037754355f4d42b4
nathalie <nathalie.metreau@hotmail.fr>
2023-08-10 01:58:02 +0200

    ex6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+#include <unistd.h>
+
+/**
+ * hash_table_print- prints a hash table.
+ * @ht:is the hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *p;
+	hash_node_t *temp;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+
+	{
+		while (i < ht->size)
+		{
+			p = ht->array[i];
+			while (p != NULL)
+			{
+				temp = p;
+				p = p->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
+			i++;
+		}
+		free(ht);
+	}
+}
