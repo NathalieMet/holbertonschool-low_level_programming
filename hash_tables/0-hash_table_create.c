d4a13436e9f03112f4e5197e93a4317f513c6c89
nathalie <nathalie.metreau@hotmail.fr>
2023-08-09 12:21:31 +0200

    ex0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,19 +17,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
 	{
-		hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-
-		if (table == NULL)
-			{
-			return (NULL);
-			}
-		table->size = size;
-		table->array = calloc(size, sizeof(hash_node_t *));
-
-		for (i = 0; i < table->size; i++)
-			table->array[i] = NULL;
-
-		return (table);
+		return (NULL);
 	}
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return (table);
 }
---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash_tables.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
