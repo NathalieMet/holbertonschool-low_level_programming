9a51551418f62de099f2ab00093dc8e633bd038b
nathalie <nathalie.metreau@hotmail.fr>
2023-08-09 12:13:02 +0200

    ex0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -20,6 +20,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
+		if (table == NULL)
+			{
+			return (NULL);
+			}
 		table->size = size;
 		table->array = calloc(size, sizeof(hash_node_t *));
 
