24b8d3aa0071f46987387cf8bd7cfd4876392f23
nathalie <nathalie.metreau@hotmail.fr>
2023-08-09 20:09:06 +0200

    ex4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,11 +9,30 @@
 #include <unistd.h>
 
 /**
- * hash_table_set- adds an element to the hash table.
+ * hash_table_get- adds an element to the hash table.
  * @ht:is the hash table
  * @key:is the key
- * @value:is the value associated with the key.
  * Return: 1 if success, 0 otherwise
  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	if (temp == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		if (!strcmp(key, temp->key))
+			return (temp->value);
+	}
+	temp = temp->next;
+	return (0);
+}
